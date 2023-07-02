171e3a46d346578a8f69a3ece7a55aac13c79278
Joaquin Enriques <joaquin.enriques@gmail.com>
2023-07-02 19:29:43 -0300

    GithubLink

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { useState } from "react"
 import DamageDisplay from "./components/DamageDisplay"
 import EnemyInput from "./components/EnemyInput"
 import FixedHpButton from "./components/FixedHpButton"
+import GithubLink from "./components/GithubLink"
 
 const App = () => {
     const [enemyHp, setEnemyHp] = useState(1000)
     console.log(enemyHp)
     return (
         <div className="app">
+            <GithubLink />
             <h3 className="heading-3 app__title">Garen ult calculator</h3>
             <section className="app__content">
                 <EnemyInput enemyHp={enemyHp} setEnemyHp={setEnemyHp}/>
